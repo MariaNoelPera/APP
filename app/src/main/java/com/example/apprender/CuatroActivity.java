42f0d8752406dc7862a42c90cdbc008a2bcbeed2
MariaNoelPera <mana0609pera@gmail.com>
2020-03-13 17:42:32 -0300

    12/03

--- app/src/main/java/com/example/apprender/CuatroActivity.java
+++ app/src/main/java/com/example/apprender/CuatroActivity.java
@@ -2,14 +2,28 @@ package com.example.apprender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CuatroActivity extends AppCompatActivity {
+    Button volver3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuatro);
+
+        volver3 = findViewById(R.id.volver3);
+        volver3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pantalla4 = new Intent(CuatroActivity.this, TresActivity.class) ;
+                startActivity(pantalla4);
+                finish();
+            }
+        });
     }
 }
