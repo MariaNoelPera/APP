17e0725fe2a28fd65fcc55f439fa02243c4b9221
MariaNoelPera <mana0609pera@gmail.com>
2020-03-13 21:52:00 -0300

    12/03

--- app/src/main/java/com/example/apprender/MainActivity.java
+++ app/src/main/java/com/example/apprender/MainActivity.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import javax.security.auth.PrivateCredentialPermission;
 
 public class MainActivity extends AppCompatActivity {
     Button botoninicio;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
