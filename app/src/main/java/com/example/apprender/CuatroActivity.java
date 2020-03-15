6307212edb6ae65b885dd6215ec87bb9b1eaa38f
MariaNoelPera <mana0609pera@gmail.com>
2020-03-14 22:25:36 -0300

    ultimo corregido

--- app/src/main/java/com/example/apprender/CuatroActivity.java
+++ app/src/main/java/com/example/apprender/CuatroActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.firebase.auth.FirebaseAuth;
 
 public class CuatroActivity extends AppCompatActivity {
     Button volver3;
